d90ac6f6783c429874cf102b1a2badd1433722c9
Thiago2281 <thiago@kubuntu1.myguest.virtualbox.org>
2024-03-30 00:24:10 -0300

    coloca process.env na base de dados

--- app/app.js
+++ app/app.js
@@ -11,9 +11,11 @@ const pool = mysql.createPool({
   host: process.env.DATABASE_HOST,
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
-  database: 'natureza_despesa'
+  database: process.env.DATABASE_DATABASE
 });
 module.exports = pool;
+
+
 const fs = require('fs');
 
 /* identificar dados passados na URL */
